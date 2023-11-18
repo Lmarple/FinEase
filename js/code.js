53680e6bd13c051c279460f391e00e755a9c027d
Luke Marple <60519906+Lmarple@users.noreply.github.com>
2023-11-18 12:30:12 -0700

    Commit Changes

---
+++ js/code.js
@@ -0,0 +1,34 @@
+window.onload = () => {
+
+    document.getElementById("AddRowButton").onclick = addRow;
+
+
+}
+
+function addRow() {
+    // Get input values
+    var expense = document.getElementById('expense').value;
+    var plannedCost = document.getElementById('plannedCost').value;
+    var actualCost = document.getElementById('actualCost').value;
+
+    // Get table reference
+    var table = document.getElementById('expenseTable').getElementsByTagName('tbody')[0];
+
+    // Create a new row
+    var newRow = table.insertRow(table.rows.length);
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    var cell3 = newRow.insertCell(2);
+
+    // Add values to the cells
+    cell1.innerHTML = expense;
+    cell2.innerHTML = plannedCost;
+    cell3.innerHTML = actualCost;
+
+    // Compare planned and actual costs to change font color
+    if (parseFloat(actualCost) > parseFloat(plannedCost)) {
+      cell3.classList.add('red');
+    } else if (parseFloat(actualCost) < parseFloat(plannedCost)) {
+      cell3.classList.add('green');
+    }
+  }
