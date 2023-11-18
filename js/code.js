4d445d5cfd0be0ed223be411b743e94f8a08cee2
Luke Marple <60519906+Lmarple@users.noreply.github.com>
2023-11-18 13:26:48 -0700

    MVP

--- js/code.js
+++ js/code.js
@@ -1,11 +1,12 @@
 window.onload = () => {
 
-    document.getElementById("AddRowButton").onclick = addRow;
-
+    document.getElementById("AddExpRowButton").onclick = addExpRow;
+    document.getElementById("AddIncRowButton").onclick = addIncomeRow;
+    document.getElementById('addSavingsRowButton').onclick = addSavingsRow;
 
 }
 
-function addRow() {
+function addExpRow() {
     // Get input values
     var expense = document.getElementById('expense').value;
     var plannedCost = document.getElementById('plannedCost').value;
@@ -35,4 +36,57 @@ function addRow() {
     document.getElementById('expense').value = '';
       document.getElementById('plannedCost').value = '';
       document.getElementById('actualCost').value = '';
+  }
+  function addIncomeRow() {
+    var income = document.getElementById('income').value;
+    var plannedAmount = document.getElementById('plannedAmount').value;
+    var actualAmount = document.getElementById('actualAmount').value;
+  
+    var tableBody = document.getElementById('incomeTable').getElementsByTagName('tbody')[0];
+  
+    var newRow = tableBody.insertRow(tableBody.rows.length);
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    var cell3 = newRow.insertCell(2);
+  
+    cell1.innerHTML = income;
+    cell2.innerHTML = plannedAmount;
+    cell3.innerHTML = actualAmount;
+  
+    if (parseFloat(actualAmount) < parseFloat(plannedAmount)) {
+      cell3.classList.add('red');
+    } else if (parseFloat(actualAmount) > parseFloat(plannedAmount)) {
+      cell3.classList.add('green');
+    }
+  
+    // Clear input fields after adding row
+    document.getElementById('income').value = '';
+    document.getElementById('plannedAmount').value = '';
+    document.getElementById('actualAmount').value = '';
+  }
+
+  function addSavingsRow() {
+    var reservedFor = document.getElementById('reservedFor').value;
+    var goalAmount = document.getElementById('goalAmount').value;
+    var currentAmount = document.getElementById('currentAmount').value;
+  
+    var tableBody = document.querySelector('#savingsTable tbody');
+  
+    var newRow = tableBody.insertRow(tableBody.rows.length);
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    var cell3 = newRow.insertCell(2);
+    var cell4 = newRow.insertCell(3);
+  
+    cell1.innerHTML = reservedFor;
+    cell2.innerHTML = goalAmount;
+    cell3.innerHTML = currentAmount;
+  
+    var leftToGo = parseFloat(goalAmount) - parseFloat(currentAmount);
+    cell4.innerHTML = leftToGo.toFixed(2);
+  
+    // Clear input fields after adding row
+    document.getElementById('reservedFor').value = '';
+    document.getElementById('goalAmount').value = '';
+    document.getElementById('currentAmount').value = '';
   }
